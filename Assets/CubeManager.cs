8424d014376bd19064c58093fdf1c335132bfdf1
min <7kyeongmin@gmail.com>
2018-12-26 15:25:10 +0900

    move_CurvePointers_Image Alpha Raycast Filter

---
+++ Assets/CubeManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeManager : MonoBehaviour {
+
+
+    public void AfterGrabbed(Draggable draggableObj)
+    {
+        Debug.Log("AfterGrabbed()");
+    }
+    public void BeforeRelease(Draggable draggableObj)
+    {
+        Debug.Log("BeforeRelease()");
+
+    }
+    public void OnDrop(Draggable draggableObj)
+    {
+        Debug.Log("OnDrop()");
+    }
+}
