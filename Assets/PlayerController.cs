854f52a3ec62f9d2ae163221816d0c98f6c5f78e
min <7kyeongmin@gmail.com>
2018-12-27 11:27:57 +0900

    IColliderEventPress 랑 Hover

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	//각각 이벤트 할당
+    public void OnVirtualPress()
+    {
+        Debug.Log("OnVirtualPress()");
+    }
+    public void OnVirtualClick()
+    {
+        Debug.Log("OnVirtualClick()");
+    }
+    public void OnVirualPressDown()
+    {
+        Debug.Log("OnVirualPressDown()");
+    }
+    public void OnVirualPressUp()
+    {
+        Debug.Log("OnVirualPressUp()");
+    }
+
+}
---
+++ Assets/PressButton.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HTC.UnityPlugin.ColliderEvent;
+
+public class PressButton : MonoBehaviour ,
+    IColliderEventPressUpHandler,
+    IColliderEventPressEnterHandler,
+    IColliderEventPressExitHandler,
+
+    IColliderEventHoverEnterHandler,
+    IColliderEventHoverExitHandler
+{
+    public GameObject pressButton;
+    public GameObject explosionEffect;
+    public GameObject explosionPosition;
+    private Transform pressButtonTransform;
+    private float pressButtonOriginalYPos;
+
+    void Start()
+    {
+        pressButtonTransform = pressButton.GetComponent<Transform>();
+        pressButtonOriginalYPos = pressButtonTransform.position.y;
+    }
+
+    public void OnColliderEventPressEnter(ColliderButtonEventData eventData)
+    {
+        pressButtonTransform.position = new Vector3(pressButtonTransform.position.x,
+            pressButtonOriginalYPos - 0.15f,
+            pressButtonTransform.position.z);
+    }
+
+    public void OnColliderEventPressExit(ColliderButtonEventData eventData)
+    {
+        pressButtonTransform.position = new Vector3(pressButtonTransform.position.x,
+            pressButtonOriginalYPos,
+            pressButtonTransform.position.z);
+    }
+
+    public void OnColliderEventPressUp(ColliderButtonEventData eventData)
+    {
+        
+    }
+
+    public void OnColliderEventHoverEnter(ColliderHoverEventData eventData)
+    {
+        pressButton.GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    public void OnColliderEventHoverExit(ColliderHoverEventData eventData)
+    {
+        pressButton.GetComponent<Renderer>().material.color = Color.red;
+    }
+}
